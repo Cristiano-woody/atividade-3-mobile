13d63d754c76dcf6d96ddff700d9b3b941afad8c
cristiano <cristiano.nog.woody@gmail.com>
2023-09-30 03:11:48 -0300

    part 1 projeto

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -3,11 +3,27 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class SenhasService {
   public senhasGeral: number = 0;
   public senhasPrior: number = 0;
   public senhasExame: number = 0;
   public senhasTotal: number = 0;
+
+  public inputNovaSenha: string = ''
+  public senhasArray: { [key: string]: string[] } = {
+    ['SP']: [],
+    ['SE']: [],
+    ['SG']: [],
+  };
+  public senhasChamadas: {  
+    tipoSenha: string;
+    guiche: string;
+    senha: string | undefined;
+    data: string;
+  }[] = [];
+  
+  constructor() { }
 
   somaGeral() { 
     this.senhasGeral++;
@@ -21,5 +37,132 @@ export class SenhasService {
     this.senhasExame++;
     this.senhasTotal++;
   }
-  constructor() { }
+  
+  novaSenha(tipoSenha: string) {
+    if (tipoSenha == 'SG') {
+      this.somaGeral();
+      if (!this.senhasArray['SG']) {
+        this.senhasArray['SG'] = [];
+      }
+      this.inputNovaSenha = 
+      this.pegarStrigData() +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SG'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SG'].push(this.inputNovaSenha);
+    } 
+    else if (tipoSenha == 'SP') {
+      this.somaPrior()
+      if (!this.senhasArray['SP']) {
+        this.senhasArray['SP'] = [];
+      }
+      this.inputNovaSenha = 
+      this.pegarStrigData() +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SP'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SP'].push(this.inputNovaSenha);
+    }
+    else if (tipoSenha == 'SE') {
+      this.somaExame()
+      if (!this.senhasArray['SE']) {
+        this.senhasArray['SE'] = [];
+      }
+      this.inputNovaSenha = 
+      this.pegarStrigData() +
+        '-' +
+        tipoSenha +
+        (this.senhasArray['SE'].length + 1).toString().padStart(2, '0');
+      this.senhasArray['SE'].push(this.inputNovaSenha);
+    }
+    console.log(this.senhasArray)
+  }
+
+  pegarStrigData(format?: boolean): string {
+    if(format) {
+      const stringDate =       
+      new Date().getDay().toString().padStart(2, '0') +
+      '/' +
+      new Date().getMonth().toString().padStart(2, '0') +
+      '/' +
+      new Date().getFullYear().toString().substring(2,4)
+      return stringDate
+    }
+    const stringDate =       
+    new Date().getDay().toString().padStart(2, '0') +
+    new Date().getMonth().toString().padStart(2, '0') +
+    new Date().getFullYear().toString().substring(2,4)
+    return stringDate
+  }
+
+  private chamarSenhaSP(guiche: string) {
+    let proxSenhaSP = this.senhasArray['SP'].shift()
+        this.senhasChamadas.unshift({
+        tipoSenha: 'SP',
+        guiche: guiche,
+        senha: proxSenhaSP,
+        data: this.pegarStrigData(true)
+      })
+  }
+
+  private  chamarSenhaSE(guiche: string) {
+    let proxSenhaSE = this.senhasArray['SE'].shift()
+    this.senhasChamadas.unshift({
+      tipoSenha: 'SE',
+      guiche: guiche,
+      senha: proxSenhaSE,
+      data: this.pegarStrigData(true)
+    })
+  }
+
+  private  chamarSenhaSG(guiche: string) {
+    let proxSenhaSG = this.senhasArray['SG'].shift()
+        this.senhasChamadas.unshift({
+          tipoSenha: 'SG',
+          guiche: guiche,
+          senha: proxSenhaSG,
+          data: this.pegarStrigData(true)
+        })
+  }
+
+  chamarSenha(guiche: string) {
+    if(this.senhasChamadas && this.senhasChamadas.length > 0) {
+      if(this.senhasChamadas[0].tipoSenha === 'SP') {
+        if (this.senhasArray['SE'] && this.senhasArray['SE'].length > 0) {
+          this.chamarSenhaSE(guiche);
+          console.log(this.senhasChamadas)
+          return
+        }
+        else if (this.senhasArray['SG'] && this.senhasArray['SG'].length > 0) {
+          this.chamarSenhaSG(guiche);
+          console.log(this.senhasChamadas)
+          return
+        }
+        else {
+          console.log('Não há mais senhas para chamar');
+          return
+        }
+      }
+    }
+    if (this.senhasArray['SP'] && this.senhasArray['SP'].length > 0) {
+      this.chamarSenhaSP(guiche);
+      console.log(this.senhasChamadas)
+      return
+    }
+    else if (this.senhasArray['SE'] && this.senhasArray['SE'].length > 0) {
+      this.chamarSenhaSE(guiche);
+      console.log(this.senhasChamadas)
+      return
+    }
+    else if (this.senhasArray['SG'] && this.senhasArray['SG'].length > 0) {
+      this.chamarSenhaSG(guiche);
+      console.log(this.senhasChamadas)
+      return
+    }
+    else {
+      console.log('Não há mais senhas para chamar');
+      return
+    }
+  }
+  
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,7 +7,6 @@ import { SenhasService } from '../services/senhas.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  inputNovaSenha: string = '';
   constructor(public senhasService: SenhasService) {}
 
 }
